f1dbdd1b07e3bb513b1201a59388b2a1d1d711b3
Venu Gopal <venu@Venus-MacBook-Pro.local>
2024-08-29 22:33:04 +0530

    arrays

--- 06-array.sh
+++ 06-array.sh
@@ -3,8 +3,8 @@
 FRUITS=("APPLE","ORANGE","KIWI","BANANA")
 
 echo "First fruit is: ${FRUITS[0]}"
-echo "First fruit is: ${FRUITS[1]}"
-echo "First fruit is: ${FRUITS[2]}"
-echo "First fruit is: ${FRUITS[3]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+echo "Fourth fruit is: ${FRUITS[3]}"
 
 echo "First fruit is: ${FRUITS[@]}"
