e05280175aadfc2e41587183228b9751465b83d7
Venu Gopal <venu@Venus-MacBook-Pro.local>
2024-08-29 22:02:20 +0530

    added conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "venu::Hi deva, how are you"
+echo "deva::hello, i am doing good , how about you"
+echo "venu:: i am good to , i am learning devops"
+echo "deva::i am learning devops too"
