ebbdae58b7e4160cd14603b7bd8f8e0ad8479a14
Venu Gopal <venu@Venus-MacBook-Pro.local>
2024-08-30 10:32:33 +0530

    added numbers

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -5,4 +5,7 @@ echo "number of variables passed: $#" #how many values given, total it will coun
 echo "script name: $0" #to know what is our script file
 echo "current working directory: $PWD" # to know our current working directory
 echo "home directory of current user: $HOME" # to know home directory of current user
-echo "process instance id(PID) of the script executing now: $$"
+echo "process instance id(PID) of the script executing now: $$" # to know the PID 
+
+sleep 100 &
+echo "PID of last background command: $!"
