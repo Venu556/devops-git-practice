bd4da8c34cc6d77f8b9756ecf7238d137ab4ddff
Venu Gopal <venu@Venus-MacBook-Pro.local>
2024-08-30 10:29:01 +0530

    added numbers

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -4,3 +4,4 @@ echo "all variables passed to the script: $@" #all values execute like 20,30 ven
 echo "number of variables passed: $#" #how many values given, total it will count
 echo "script name: $0" #to know what is our script file
 echo "current working directory: $PWD" # to know our current working directory
+echo "home directory of current user: $HOME"
