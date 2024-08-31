7ca66a05469bdaaf329eeb2351e7b87ee7d99925
Venu Gopal <venu@Venus-MBP.lan>
2024-08-31 12:27:41 +0530

    deleted files

--- devops-git-practice/02-conversation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "venu::Hi deva, how are you"
-echo "deva::hello, i am doing good , how about you"
-echo "venu:: i am good to , i am learning devops"
-echo "deva::i am learning devops too"
